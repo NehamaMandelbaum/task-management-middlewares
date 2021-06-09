28a791d0fe6735bd62ae1fceb2b2f470f7b2d558
NehamaMandelbaum <nehama1836@gmail.com>
2021-06-09 14:55:24 -0300

    erros nos testes

--- src/index.js
+++ src/index.js
@@ -10,19 +10,64 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+
+  const user = users.find((user) => user.username === username)
+
+  if(!user){
+    return response.status(404).json({error: "User not found!"})
+  }
+  request.user = user
+
+  return next(); 
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request; 
+  if(user.pro === false && user.todos.length > 9){
+    response.status(403).send()
+  }
+  return next(); 
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers
+  const {id} = request.params
+
+  const user = users.find((user) => user.username === username)
+
+  if(!user){
+    return response.status(404).json({error: "User not found!"})
+  }
+  request.user = user
+
+  if (id.validate === false){
+    response.status(400).json()
+  }
+
+  const todo = user.todos.find((todo) => todo.id === id)
+
+  if(!todo){
+    response.status(404).json()
+  }
+
+  request.todo = todo
+
+return next(); 
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params
+
+  const user = users.find((user) => user.id === id)
+
+  if(!user){
+    response.status(404).json(); 
+  }
+
+  request.user = user
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
